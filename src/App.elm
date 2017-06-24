cca7c1f9b60f1dfce4982ea86be0a6fbe115cc66
Darren Prentice <>
2017-06-23 22:00:33 -0500

    mouse pos example

--- src/App.elm
+++ src/App.elm
@@ -4,6 +4,7 @@ import Html exposing (Html, text, div)
 import Json.Encode exposing (Value)
 import Lib exposing (..)
 import Encode exposing (encodeGraph)
+import Mouse exposing (Position)
 
 
 ---- PORT ----
@@ -16,16 +17,18 @@ port renderContextJs : Value -> Cmd msg
 ---- AUDIO GRAPH ----
 
 
-jsAudioGraph =
-    encodeGraph audioGraph
+renderAudioGraph : Model -> Cmd msg
+renderAudioGraph =
+    renderContextJs << encodeGraph << audioGraph
 
 
-audioGraph : AudioContextGraph
-audioGraph =
-    [ ( osc "osc1" [ oscType Sine, frequency 440 ], [ "gain1" ] )
-    , ( osc "osc2" [ oscType Sawtooth, frequency 220 ], [ "gain2" ] )
-    , ( gain "gain1" [ volume 0.03 ], [ "output" ] )
-    , ( gain "gain2" [ volume 0.01 ], [ "output" ] )
+audioGraph : Model -> AudioContextGraph
+audioGraph model =
+    [ ( osc "osc1" [ oscType Sine, frequency model.x ], [ "gain1" ] )
+    , ( osc "osc2" [ oscType Sawtooth, frequency model.x ], [ "gain2" ] )
+    , ( gain "gain1" [ volume 0.03 ], [ "master" ] )
+    , ( gain "gain2" [ volume 0.01 ], [ "master" ] )
+    , ( gain "master" [ volume (model.y * 0.0008) ], [ "output" ] )
     ]
 
 
@@ -36,34 +39,59 @@ audioGraph =
 main : Program Never Model Msg
 main =
     Html.program
-        { view = view
-        , init = init
+        { init = init
+        , view = view
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = subscriptions
         }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { x : Float
+    , y : Float
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { x = 0, y = 0 }, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Position Int Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Position x y ->
+            ( { model | x = toFloat x, y = toFloat y }, renderAudioGraph model )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Mouse.moves (\{ x, y } -> Position x y)
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ div [] [ text model.message ]
-        , div [] [ text (toString audioGraph) ]
+        [ div [] [ text ("x: " ++ (toString model.x) ++ "  y: " ++ (toString model.y)) ]
+        , div [] [ text (toString <| audioGraph model) ]
         ]
-
-
-type alias Model =
-    { message : String }
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( { message = "audio node graph:" }, (renderContextJs jsAudioGraph) )
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import './main.css'
-const Elm = require('./App.elm')
+const Elm = require('./App.elm');
 const createVirtualAudioGraph = require('virtual-audio-graph');
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 if (typeof window.AudioContext === 'undefined') {
-    document.write('Sorry, this browser does not support the Web Audio API.');
+  alert('Sorry, this browser does not support the Web Audio API.');
 }
 
-
-const root = document.getElementById('root')
-const app = Elm.App.embed(root)
+const root = document.getElementById('root');
+const app = Elm.App.embed(root);
 
 const audioContext = new AudioContext();
 const virtualAudioGraph = createVirtualAudioGraph({
@@ -18,8 +17,8 @@ const virtualAudioGraph = createVirtualAudioGraph({
 });
 
 app.ports.renderContextJs.subscribe(function(graph) {
-    console.info(graph);
-    virtualAudioGraph.update(graph);
+  // console.info(graph);
+  virtualAudioGraph.update(graph);
 });
 
-window.v = virtualAudioGraph
+window.v = virtualAudioGraph;
